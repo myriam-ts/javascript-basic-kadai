c510cf280ac09b24e57bcd62db865f21cecbfad7
myriam-ts <150800309+myriam-ts@users.noreply.github.com>
2024-01-02 17:01:19 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,10 @@
+//btnというidを持つHTML要素を取得し、定数に代入
+const btn = document.getElementById('btn');
+
+//textというidを持つHTML要素を取得し、定数に代入
+const text = document.getElementById('text');
+
+//btnがクリックされたときにtextが変わるイベント処理を実行
+btn.addEventListener('click',()=>{
+    text.textContent='ボタンをクリックしました';
+})
